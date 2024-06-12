6be76561cfb3554a98a429950129ac977d782701
quachnamluong <quachnamluong@gmail.com>
2024-06-12 22:03:19 +0700

    update

--- PhoneShopApi.Product/Controllers/PhoneColorController.cs
+++ PhoneShopApi.Product/Controllers/PhoneColorController.cs
@@ -76,7 +76,7 @@ namespace PhoneShopApi.Product.Controllers
             }
             catch (Exception ex )
             {
-                return $"{Request.Scheme}://{Request.Host}/Uploads/PhoneImages/NotFound.jpg";
+                //return $"{Request.Scheme}://{Request.Host}/Uploads/PhoneImages/NotFound.jpg";
             }
                         
             return Path.Combine("Uploads", "PhoneImages", filename);
